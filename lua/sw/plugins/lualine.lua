aa2339d7d9dd007b90c7dd5bb5b2226def5c82c1
szymonwrobel <szymon.wrobel@r.pl>
2025-09-09 17:31:34 +0200

    feat: add showing relative path to lualine, not only the filename

--- lua/sw/plugins/lualine.lua
+++ lua/sw/plugins/lualine.lua
@@ -42,7 +42,7 @@ return {
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename', get_current_date_time() },
+        lualine_c = { { 'filename', path = 1 }, get_current_date_time() },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
@@ -50,7 +50,7 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'filename' },
+        lualine_c = { { 'filename', path = 1 } },
         lualine_x = { 'location' },
         lualine_y = {},
         lualine_z = {}
