4ce61ddbb7eb9a886db4e241d40b8952f532685b
szymonwrobel <szymon.wrobel@r.pl>
2025-09-09 17:28:34 +0200

    feat: add add transparency to colorscheme

--- lua/sw/plugins/colorscheme.lua
+++ lua/sw/plugins/colorscheme.lua
@@ -25,7 +25,11 @@ return {
     "olimorris/onedarkpro.nvim",
     priority = 1000, -- Ensure it loads first
     config = function()
-      require("onedarkpro").setup({})
+      require("onedarkpro").setup({
+        options = {
+          transparency = true
+        }
+      })
       ColorMyPencils()
     end
   }
