a78e0de17731f6c281419fef22e7d3b9455beec0
szymonwrobel <szymon.wrobel@r.pl>
2025-11-01 00:15:10 +0100

    feat: add keymaps for disabling and enabling copilot

--- lua/sw/plugins/copilot.lua
+++ lua/sw/plugins/copilot.lua
@@ -1,1 +1,9 @@
-return { "github/copilot.vim" }
+return {
+  "github/copilot.vim",
+  config = function()
+    vim.keymap.set("n", "<leader><leader>ce", "<cmd>Copilot enable<CR>",
+      { noremap = true, silent = true, desc = "Enable Copilot" })
+    vim.keymap.set("n", "<leader><leader>cd", "<cmd>Copilot disable<CR>",
+      { noremap = true, silent = true, desc = "Disable Copilot" })
+  end
+}
