6fa096064609a5a1cd5f68bb7a9d72e06c9e1e3e
szymonwrobel <szymon.wrobel@r.pl>
2026-04-14 11:23:37 +0200

    chore: bump lazy packages versions

--- lua/sw/plugins/copilot.lua
+++ lua/sw/plugins/copilot.lua
@@ -1,9 +1,10 @@
-return {
-  "github/copilot.vim",
-  config = function()
-    vim.keymap.set("n", "<leader><leader>ce", "<cmd>Copilot enable<CR>",
-      { noremap = true, silent = true, desc = "Enable Copilot" })
-    vim.keymap.set("n", "<leader><leader>cd", "<cmd>Copilot disable<CR>",
-      { noremap = true, silent = true, desc = "Disable Copilot" })
-  end
-}
+return {}
+-- return {
+--   "github/copilot.vim",
+--   config = function()
+--     vim.keymap.set("n", "<leader><leader>ce", "<cmd>Copilot enable<CR>",
+--       { noremap = true, silent = true, desc = "Enable Copilot" })
+--     vim.keymap.set("n", "<leader><leader>cd", "<cmd>Copilot disable<CR>",
+--       { noremap = true, silent = true, desc = "Disable Copilot" })
+--   end
+-- }
